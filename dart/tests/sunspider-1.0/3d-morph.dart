5d6cb3d8070c5ffcce5ac23b203733b6eb55de5d
vvakame <vvakame+dev@gmail.com>
2012-07-11 12:00:41 +0900

    create tests/sunspider-1.0/3d-morph.dart

--- dart/tests/sunspider-1.0/3d-morph.dart
+++ dart/tests/sunspider-1.0/3d-morph.dart
@@ -23,32 +23,35 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-var loops = 15
-var nx = 120
-var nz = 120
+const int loops = 15;
+const int nx = 120;
+const int nz = 120;
 
-function morph(a, f) {
-    var PI2nx = Math.PI * 8/nx
-    var sin = Math.sin
-    var f30 = -(50 * sin(f*Math.PI*2))
-    
-    for (var i = 0; i < nz; ++i) {
-        for (var j = 0; j < nx; ++j) {
-            a[3*(i*nx+j)+1]    = sin((j-1) * PI2nx ) * -f30
-        }
-    }
+void morph(List<num> a, num f) {
+  var PI2nx = Math.PI * 8/nx;
+  var sin = Math.sin;
+  var f30 = -(50 * sin(f*Math.PI*2));
+  
+  for (var i = 0; i < nz; ++i) {
+      for (var j = 0; j < nx; ++j) {
+          a[3*(i*nx+j)+1]    = sin((j-1) * PI2nx ) * -f30;
+      }
+  }
 }
 
+void main() {
     
-var a = Array()
-for (var i=0; i < nx*nz*3; ++i) 
-    a[i] = 0
+  var a = new List<int>();
+  for (int i=0; i < nx*nz*3; ++i) 
+      a[i] = 0;
+  
+  for (var i = 0; i < loops; ++i) {
+      morph(a, i/loops);
+  }
+  
+  var testOutput = 0;
+  for (var i = 0; i < nx; i++)
+      testOutput += a[3*(i*nx+i)+1];
+  a = null;
 
-for (var i = 0; i < loops; ++i) {
-    morph(a, i/loops)
 }
-
-testOutput = 0;
-for (var i = 0; i < nx; i++)
-    testOutput += a[3*(i*nx+i)+1];
-a = null;
