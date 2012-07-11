ce830edf2448b75a998fd2d3edab0830aecd8bec
vvakame <vvakame+dev@gmail.com>
2012-07-11 15:03:52 +0900

    create tests/sunspider-1.0/bitops-3bit-bits-in-byte.dart

--- dart/tests/sunspider-1.0/bitops-3bit-bits-in-byte.dart
+++ dart/tests/sunspider-1.0/bitops-3bit-bits-in-byte.dart
@@ -4,13 +4,14 @@ var result = 0;
 
 // 1 op = 6 ANDs, 3 SHRs, 3 SHLs, 4 assigns, 2 ADDs
 // O(1)
-function fast3bitlookup(b) {
-var c, bi3b = 0xE994; // 0b1110 1001 1001 0100; // 3 2 2 1  2 1 1 0
-c  = 3 & (bi3b >> ((b << 1) & 14));
-c += 3 & (bi3b >> ((b >> 2) & 14));
-c += 3 & (bi3b >> ((b >> 5) & 6));
-return c;
 
+num fast3bitlookup(num b) {
+  var c, bi3b = 0xE994; // 0b1110 1001 1001 0100; // 3 2 2 1  2 1 1 0
+  c  = 3 & (bi3b >> ((b << 1) & 14));
+  c += 3 & (bi3b >> ((b >> 2) & 14));
+  c += 3 & (bi3b >> ((b >> 5) & 6));
+  return c;
+  
 /*
 lir4,0xE994; 9 instructions, no memory access, minimal register dependence, 6 shifts, 2 adds, 1 inline assign
 rlwinmr5,r3,1,28,30
@@ -24,13 +25,14 @@ addr3,r3,r10
 */
 }
 
-
-function TimeFunc(func) {
-var x, y, t;
-var sum = 0;
-for(var x=0; x<500; x++)
-for(var y=0; y<256; y++) sum += func(y);
-return sum;
+num TimeFunc(num func(number)) {
+  var x, y, t;
+  var sum = 0;
+  for(x=0; x<500; x++)
+  for(y=0; y<256; y++) sum += func(y);
+  return sum;
 }
 
-sum = TimeFunc(fast3bitlookup);
+void main() {
+  var sum = TimeFunc(fast3bitlookup);
+}
